07b2bea27166593fdd4fc6e33b00bfb2c515ce62
MRLmic <missi.luna@gmail.com>
2017-09-23 15:02:08 -0400

    User ID added to Schema

---
+++ scripts/songs/create.sh
@@ -0,0 +1,19 @@
+
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="songs"
+curl "${API}${URL_PATH}" \
+--include \
+--request POST \
+"http://localhost:4741/songs" \
+--header "Content-type: application/json" \
+--data '{
+  "song": {
+    "title": "'"${TITLE}"'",
+    "performed_by": "'"${PERFORMED_BY}"'",
+    "written_by": "'"${WRITTEN_BY}"'",
+    "year_performed": "'"${YEAR_PERFORMED}"'"
+  }
+}'
+
+echo
